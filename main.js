5b8d6cdb776c4c2e6898ef9e1de225cb032d9c09
Richard Brasil <82055788+richardbrasil1@users.noreply.github.com>
2024-02-28 11:42:48 -0300

    inclusão da condição do evento no teclado através do operador lógico OR

--- main.js
+++ main.js
@@ -4,19 +4,28 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
+//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];   
-    const idAudio = `#som_${instrumento}`; //template string
-     
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string     
 
-    tecla.onclick = function (){
+    tecla.onclick = function () {
         tocaSom(idAudio);
-    }     
-       
+    }
 
-    console.log(contador);
-} 
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+    //console.log(contador);
+}
 
